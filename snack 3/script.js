51d2bb975351ee8fcb6f7c90441dd151ad9f67b6
chrialge <chrialge@gmail.com>
2024-03-05 15:52:10 +0100

    add snack 3

--- snack 3/script.js
+++ snack 3/script.js
@@ -9,3 +9,29 @@ Es:
 ]
 Crea un nuovo array con la lista dei mammiferi.
 */
+const animals = [
+  { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+  { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'gallina', famiglia: 'fasionadi', classe: 'uccelli' },
+  { nome: 'delfino', famiglia: 'delphinade', classe: 'mammiferi' },
+  { nome: 'pinguino', famiglia: 'sfeniscidi', classe: 'uccelli' },
+  { nome: 'foca', famiglia: 'focidii', classe: 'mammiferi' },
+  { nome: 'balena', famiglia: 'balenidi', classe: 'pesci' },  
+  { nome: 'rana toro', famiglia: 'ranidi', classe: 'anfibi' },
+  { nome: 'chiodino', famiglia: 'physalacriaceae', classe: 'fungo' }
+];
+
+console.log(animals)
+
+
+// forma estesa
+// const mammiferi = animals.filter(function(animal){
+//   if(animal.classe === 'mammiferi'){
+//     return true
+//   }
+// })
+// console.log(mammiferi)
+
+// forma contratta
+const mammiferi = animals.filter((animal) => animal.classe === 'mammiferi' )
+console.log(mammiferi)
