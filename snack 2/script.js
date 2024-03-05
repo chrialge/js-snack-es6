8a84b1b0f805c16fe096da369eef7307f235da7f
chrialge <chrialge@gmail.com>
2024-03-05 13:25:29 +0100

    add repo and comments

--- snack 2/script.js
+++ snack 2/script.js
@@ -1,3 +1,7 @@
+/*
+A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’]
+*/
 const array = [
     'cIAO',
     'pluto',
